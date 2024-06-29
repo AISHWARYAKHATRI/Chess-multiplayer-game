5927cfe0881ec6eb77d128d3651fd69abb4c2309
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2024-06-29 20:53:40 +0530

    Multiplayer chess game project backend setup

---
+++ client/readme.md
@@ -0,0 +1,1 @@
+# Multiplaye chess game client
---
+++ server/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: null,
+      database: 'multiplayer_chess_game',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
