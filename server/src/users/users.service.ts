b0610d641a8fb838fc9512aa3c86137358d0258c
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2024-06-30 11:13:03 +0530

    Email username already exist validation

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/users.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/users.dto';
 import * as bcrypt from 'bcrypt';
+import { USER } from 'src/shared/constants/response-messages';
 
 @Injectable()
 export class UsersService {
@@ -11,19 +12,47 @@ export class UsersService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  async createNewUser(createUserDto: CreateUserDto) {
-    try {
-      const { password } = createUserDto;
-      const hashedPassword = await bcrypt.hash(password, 10);
+  async createNewUser(user: CreateUserDto) {
+    const { password, email, username } = user;
 
-      const newUser = this.userRepository.create({
-        ...createUserDto,
-        password: hashedPassword,
-      });
+    const existingEmail = await this.userRepository.findOne({
+      where: { email: email },
+    });
 
-      return this.userRepository.save(newUser);
-    } catch (error) {
-      return error;
+    // check if email already exists
+    if (existingEmail) {
+      throw new HttpException(
+        USER.ErrorMessages.USER_EMAIL_ALREADY_EXIST,
+        HttpStatus.BAD_REQUEST,
+      );
     }
+
+    // Check if the username already exists
+    const existingUsername = await this.userRepository.findOne({
+      where: { username: username },
+    });
+
+    if (existingUsername) {
+      throw new HttpException(
+        USER.ErrorMessages.USERNAME_ALREADY_EXIST,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const newUser = this.userRepository.create({
+      ...user,
+      password: hashedPassword,
+    });
+
+    await this.userRepository.save(newUser);
+
+    return {
+      status: HttpStatus.CREATED,
+      message: 'User registered',
+    };
   }
 }
