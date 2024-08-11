ebfb9ad1ad6c774154e1691bbf22ed110352383d
AISHWARYAKHATRI <191200107021@asoit.edu.in>
2024-08-11 22:14:37 +0530

    Favicon and logo color change

--- client/src/app/layouts/AuthLayout.tsx
+++ client/src/app/layouts/AuthLayout.tsx
@@ -15,9 +15,7 @@ const AuthLayout: React.FC<Props> = ({ children }) => {
         <section className="bg-[rgba(5,5,5,0.5)] rounded-[12px] border-2 border-gray-800 w-[400px] p-8 flex justify-center items-center flex-col">
           <header className="flex items-end justify-center mb-9">
             <img src={Logo.src} alt="Chess" className="w-[35px]" />
-            <h1 className="text-5xl text-cgreen-300 font-bold kanit">
-              ChessMate
-            </h1>
+            <h1 className="text-5xl text-white font-bold kanit">ChessMate</h1>
           </header>
           <div className="w-full">{children}</div>
         </section>
